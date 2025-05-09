48b65ffd56386d71857be60b22e5f8f8d2403181
bossacode <hlee2745@gmail.com>
2025-05-09 19:09:18 +0900

    Loss functions

--- utils/loss_fn.py
+++ utils/loss_fn.py
@@ -4,21 +4,13 @@ from utils.tda import TakensEmbedding, DTMLayer, CubicalPL
 
 
 class LossFn:
-    def __init__(self, mode):
-        """_summary_
-
-        Args:
-            mode (str): _description_
-
-        Raises:
-            KeyError: _description_
-        """
-        if mode == "L1":
+    def __init__(self, cfg):
+        if cfg.loss_fn == "L1":
             self.loss_fn = nn.L1Loss()
-        elif mode == "L2":
+        elif cfg.loss_fn == "L2":
             self.loss_fn = nn.MSELoss()
         else:
-            raise KeyError('"mode" must be one of "L1" or "L2".')
+            raise KeyError('"loss_fn" must be one of "L1" or "L2".')
     
     def __call__(self, y_pred, y):
         loss = self.loss_fn(y_pred, y)
@@ -26,46 +18,33 @@ class LossFn:
 
 
 # loss function including MIP regularization
-class MipLossFn(LossFn):
-    def __init__(self, mode, cfg, model):
-        """_summary_
+# class MipLossFn(LossFn):
+#     def __init__(self, cfg, model):
+#         """_summary_
 
-        Args:
-            mode (str): _description_
-            cfg (_type_): _description_
-            model (_type_): _description_
+#         Args:
+#             cfg (_type_): _description_
+#             model (_type_): _description_
 
-        Raises:
-            KeyError: _description_
-        """
-        super().__init__(mode)
-        self.mode = mode
-        self.cfg = cfg
-        self.model = model
+#         Raises:
+#             KeyError: _description_
+#         """
+#         super().__init__(cfg)
+#         self.cfg = cfg
+#         self.model = model
     
-    def __call__(self, y_pred, y):
-        loss = self.loss_fn(y_pred, y)
-        # MIP
-        reg = torch.tensor(0) if round(self.model.k.item()) == self.cfg.k else (self.model.k - self.cfg.k)**2
-        print(f"{self.mode} Loss:", loss.item(), "Regularization:", self.cfg.lamda * reg.item())
-        return loss + (self.cfg.lamda * reg)
+#     def __call__(self, y_pred, y):
+#         loss = self.loss_fn(y_pred, y)
+#         # MIP
+#         reg = torch.tensor(0) if round(self.model.k.item()) == self.cfg.k else (self.model.k - self.cfg.k)**2
+#         print(f"{self.cfg.loss_fn} Loss:", loss.item(), "Regularization:", self.cfg.lamda * reg.item())
+#         return loss + (self.cfg.lamda * reg)
 
 
-# loss function including TDA Loss
+# loss function with TDA regularization
 class TdaLossFn(LossFn):
-    def __init__(self, mode, cfg, pl_target):
-        """_summary_
-
-        Args:
-            mode (str): _description_
-            cfg (_type_): _description_
-            pl_target (_type_): _description_
-
-        Raises:
-            KeyError: _description_
-        """
-        super().__init__(mode)
-        self.mode = mode
+    def __init__(self, cfg, pl_target):
+        super().__init__(cfg)
         self.cfg = cfg
         self.embed = TakensEmbedding(time_delay=cfg.time_delay, dimension=cfg.dimension, stride=cfg.stride)
         self.dtm = DTMLayer(cfg.m0, cfg.lims, cfg.size)
@@ -74,45 +53,37 @@ class TdaLossFn(LossFn):
     
     def __call__(self, y_pred, y):
         loss = self.loss_fn(y_pred, y)
-        # TDA 
+        # TDA
         pl_pred = self.pllay(self.dtm(self.embed(y_pred)))
         tda_loss = self.loss_fn(pl_pred, self.pl_target)
-        print(f"{self.mode} Loss:", loss.item(), "TDA Loss:", self.cfg.alpha * tda_loss.item())
+        print(f"{self.cfg.loss_fn} Loss:", loss.item(), "TDA Loss:", self.cfg.alpha * tda_loss.item())
         return loss + (self.cfg.alpha * tda_loss)
 
 
-# class TdaMipLossFn(MipLoss):
-#     def __init__(self, mode, cfg, model=None):
-#         super().__init__(mode, cfg, model)
-#         self.embed = TakensEmbedding(
-#             time_delay=cfg.time_delay,
-#             dimension=cfg.dimension,
-#             stride=cfg.stride
-#             )
-#         ############################################################
-#         # change parameters here
-#         self.dtm = DTMLayer()
-#         self.pl = CubicalPL()
-#         ############################################################
+# loss function with TDA regularization and scaling to complement transaction cost
+class TdaScaledLossFn(LossFn):
+    def __init__(self, cfg, model, pl_target, w_prev):
+        super().__init__(cfg)
+        self.cfg = cfg
+        self.model = model
+        self.embed = TakensEmbedding(time_delay=cfg.time_delay, dimension=cfg.dimension, stride=cfg.stride)
+        self.dtm = DTMLayer(cfg.m0, cfg.lims, cfg.size)
+        self.pllay = CubicalPL(cfg.constr, cfg.sublevel, cfg.interval, cfg.steps, cfg.K_max, cfg.dimensions)
+        self.pl_target = pl_target
+        self.w_prev = w_prev
     
-#     def __call__(self, y_pred, y):
-#         loss = self.loss_fn(y_pred, y)
-
-#         # TDA 
-#         y_pred_embed = self.embed(y_pred)
-#         y_pred_dtm = self.dtm(y_pred_embed)
-#         pl_pred = self.pl(y_pred_dtm)
-        
-#         y_embed = self.embed(y)
-#         y_dtm = self.dtm(y_embed)
-#         pl = self.pl(y_dtm)
-
-#         tda_loss = self.loss_fn(pl_pred, pl)    ######## need to give weight to this as well
-
-#         # regularization on number of assets
-#         if self.model is not None:
-#             regularization = torch.tensor(0) if round(self.model.k.item()) == self.cfg.k else self.cfg.lamda * (self.model.k - self.cfg.k)**2
-#             print(f"{self.mode} Loss:", loss.item(), "TDA Loss:", tda_loss.item(), "Regularization:", regularization.item())
-#             return loss + tda_loss + regularization
-#         else:
-#             return loss + tda_loss
+    def __call__(self, y_pred, y):
+        w = torch.softmax(self.model.w, dim=0)
+        buy = torch.where(w > self.w_prev, 1., 0.)  # indicator representing whether each stock was bought(=1) or sold/no transaction(=0)
+        transaction_prop = torch.sum((self.cfg.fcb*buy - self.cfg.fcs*(1-buy)) * (w - self.w_prev)) # proportion of transaction cost in total value of our porfolio, i.e., G_t / C_t
+        ##########################################################################################################
+        # use double of actual transaction cost
+        # transaction_prop = torch.sum((2*self.cfg.fcb*buy - 2*self.cfg.fcs*(1-buy)) * (w - self.w_prev))  # proportion of transaction cost in total value of our porfolio, i.e., G_t / C_t
+        ##########################################################################################################
+        scale = torch.pow(1/(1-transaction_prop), 1/self.cfg.pred_window_size)   # scale to overestimate y
+        loss = self.loss_fn(y_pred, torch.log(scale) + y)
+        # TDA
+        pl_pred = self.pllay(self.dtm(self.embed(y_pred)))
+        tda_loss = self.loss_fn(pl_pred, self.pl_target)
+        print(f"{self.cfg.loss_fn} Loss:", loss.item(), "TDA Loss:", self.cfg.alpha * tda_loss.item())
+        return loss + (self.cfg.alpha * tda_loss)
